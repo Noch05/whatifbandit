1eba59560dbdb0df9634205725889de782459791
Noah Ochital <noahochital@icloud.com>
2025-07-04 11:47:51 -0400

    Updating Documentation; accepting cols as strings for ease of use

--- R/single_mab_simulation.R
+++ R/single_mab_simulation.R
@@ -7,15 +7,14 @@
 #' Hadad et al. (2021). This function wraps around [run_mab_trial()] and [get_adaptive_aipw()],
 #' completing the full MAB pipeline: treatment assignment, success imputation, and estimation.
 #'
-#
 #'
-#' @param data A data frame that provides the input data for the trial.
-#' @param time_unit A string specifying the unit of time for assigning periods.
+#' @param data A data frame, tibble or data.table that provides the input data for the trial.
+#' @param time_unit A string specifying the unit of time for assigning periods when 'assignment_method` is 'date'
 #'                  Acceptable values are "Day", "Week", or "Month".
 #' @param perfect_assignment Logical; if TRUE, assumes perfect information for treatment assignment
 #'                           (i.e., all outcomes are observed regardless of the date).
-#'                           If FALSE, hides outcomes not yet theoretically observed, based on
-#'                           `letter_sent_date` from the `tanf` dataset.
+#'                           If FALSE, hides outcomes not yet theoretically observed, based
+#'                           on the dates treatments would have been assigned for each wave.
 #' @param algorithm A string specifying the MAB algorithm to use. Options are "Thompson" or "UCB1".
 #' @param period_length Numeric; length of each treatment period.
 #' If assignment method is "Date", this refers to the
@@ -26,24 +25,26 @@
 #' @param whole_experiment Logical; if TRUE, uses all past experimental data for imputing outcomes.
 #'                         If FALSE, uses only data available up to the current period.
 #' @param conditions Named Character vector containing treatment conditions, Control condition, must be named "Control".
+#' @param data_cols Named List containing the names of columns in data as strings:
+#' \itemize{
+#' \item{id:} {Column in data, contains unique id as a key}
+#' \item{success:} {Column in data; Binary successes from original experiment}
+#' \item{date:} {Column in data, contains original date of event/trial; only ncessary when assigning by 'Date'}
+#' \item{month:} {Column in data, contains month of treatment; only necessary when time_unit = 'Month'}
+#' \item{success_date:} {Column in data, contains original dates each success occured; only necessary when 'perfect_assignment' = FALSE}
+#' \item{assignment_date:} {Column in data, contains original dates treatments are assigned to observations; only necessary when 'perfect_assignment' = FALSE.
+#' Used to simulate imperfect information on part of researchers conducting an adaptive trial}}
 #' @param blocking Logical; Whether or not to use treatment blocking.
 #' @param block_cols Character Vector of variables to block by.
-#' @param date_col Column in data, contains date of treatment.
-#' @param month_col Column in data, contains month of treatment.
-#' @param id_col Column in data, contains unique id as a key.
-#' @param condition_col Column in data, contains original treatment conditions.
-#' @param success_col Column in data, contains binary successes.
-#' @param success_date_col Column in data, contains date each success occurred.
-#' @param assignment_date_col Column in data, contains date of wave assignment.
-#' @param verbose Logical; Whether or not to print iteration number. FALSE by default.
 #' @param assignment_method String; "Date" or "Batch" to define the assignment into treatment waves.
 #' @param control_augment Number \eqn{\in} \[0,1\]; Proportion of each wave guaranteed to get "Control" treatment.
-#' Default is 0. Creates potential conflicts with [randomizr::complete_ra()], when not set to 0.
-#'
+#' Default is 0.
+#' @param verbose Logical; Whether or not to print iteration number. FALSE by default.
 #'
 #' @return  A custom mab class object, which is a named list containing:
 #' \item{final_data}{The processed data with treatment assignments and imputed outcomes, labelled with "mab_" prefix.}
 #' \item{bandits}{Either the UCB1 statistics or Thompson Sampling posterior distributions.}
+#' \item{assignment_probs}{Probability of being assigned each treatment arm at a given period}
 #' \item{estimates}{AIPW (Augmented Inverse Probability Weighting) treatment effect estimates and variances.}
 #' \item{settings}{A list of the configuration settings used in the trial.}
 #'
@@ -56,25 +57,24 @@
 #' @export
 
 single_mab_simulation <- function(data,
-                                  time_unit = NULL,
-                                  perfect_assignment,
+                                  assignment_method,
                                   algorithm,
-                                  period_length = NULL,
+                                  conditions,
                                   prior_periods,
                                   whole_experiment,
-                                  conditions,
                                   blocking,
+                                  data_cols,
+                                  control_augment = 0,
+                                  time_unit = NULL,
+                                  period_length = NULL,
                                   block_cols = NULL,
-                                  date_col = NULL,
-                                  month_col = NULL,
-                                  id_col,
-                                  condition_col,
-                                  success_col,
-                                  success_date_col = NULL,
-                                  assignment_date_col = NULL,
-                                  verbose = FALSE, assignment_method,
-                                  control_augment = 0) {
+                                  verbose = FALSE) {
   data_name <- deparse(substitute(data))
+  col_names <- date_cols
+  col_syms <- date_cols
+  block_names <- block_cols
+  block_syms <- syms(block_cols)
+
   # Input Validation
 
 
@@ -149,3 +149,13 @@ single_mab_simulation <- function(data,
 
   return(results)
 }
+#' @noRd
+#' @name cols
+#' @noRD
+#' @param id Column in data, contains unique id as a key
+#' @param success  Column in data; Binary successes from original experiment
+#' @param date  Column in data, contains original date of event/trial; only ncessary when assigning by 'Date'
+#' @param month  Column in data, contains month of treatment; only necessary when time_unit = 'Month'
+#' @param success_date  Column in data, contains original dates each success occured; only necessary when 'perfect_assignment' = FALSE
+#' @param assignment_date  Column in data, contains original dates treatments are assigned to observations; only necessary when 'perfect_assignment' = FALSE.
+#'
