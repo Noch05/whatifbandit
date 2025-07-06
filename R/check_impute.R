a11657b6c0820cdeb8bf365d4775361b47a30acb
Noah Ochital <noahochital@icloud.com>
2025-07-06 16:46:27 -0400

    Finished method expansion for check_impute

--- R/check_impute.R
+++ R/check_impute.R
@@ -7,27 +7,29 @@
 #' @inheritParams get_past_results
 #' @param imputation_information `success` element of the `imputation_information`
 #' list created by [imputation_prep()].
-
-
 check_impute <- function(imputation_information, current_data) {
+  base::UseMethod("check_impute")
+}
+#' @method check_impute tbl_df
+#' @title [check_impute()] for tibbles
+#' @inheritParams check_impute
+
+check_impute.tbl_df <- function(imputation_information, current_data) {
   mean_rate <- base::mean(imputation_information$success_rate)
-  current_blocks <- current_data$impute_block[current_data$impute_req == 1]
-  imputation_blocks <- imputation_information$treatment_block
 
-  missing_blocks <- stats::na.omit(
-    base::setdiff(current_blocks, imputation_blocks)
-  )
+  current_blocks <- stats::na.omit(current_data$impute_block[current_data$impute_req == 1])
+  imputation_blocks <- stats::na.omit(imputation_information$treatment_block)
 
-  blocks_to_remove <- stats::na.omit(
-    base::setdiff(imputation_blocks, current_blocks)
-  )
+  missing_blocks <- base::setdiff(current_blocks, imputation_blocks)
+
+  blocks_to_remove <- base::setdiff(imputation_blocks, current_blocks)
 
   if (base::length(missing_blocks) > 0) {
     addition <- tibble::tibble(
       treatment_block = missing_blocks,
-      success_rate = mean_rate
+      success_rate = mean_rate,
+      failure_rate <- 1 - mean_rate
     )
-    addition$failure_rate <- 1 - addition$success_rate
 
     imputation_information <- dplyr::bind_rows(imputation_information, addition)
   }
@@ -40,4 +42,22 @@ check_impute <- function(imputation_information, current_data) {
 
 
   return(imputation_information)
+}
+#-------------------------------------------------------------------------------
+#' @method check_impute data.frame
+#' @title [check_impute()] for data.frames
+#' @inheritParams check_impute
+check_impute.data.frame <- function(imputation_information, current_data) {
+  return(
+    check_impute.tbl_df(
+      tibble::as_tibble(imputation_information),
+      tibble::as_tibble(current_data)
+    )
+  )
+}
+#' @method check_impute data.table
+#' @title [check_impute()] for data.tables
+#' @inheritParams check_impute
+check_impute.data.table <- function(imputation_information, current_data) {
+  return(0)
 }
