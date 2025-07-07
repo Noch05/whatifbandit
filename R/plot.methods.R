0b162ba09516dd287246538097b047ee74b78562
Noah Ochital <noahochital@icloud.com>
2025-07-07 19:47:20 -0400

    plot methods done

--- R/plot.methods.R
+++ R/plot.methods.R
@@ -136,8 +136,8 @@ plot_estimates <- function(x, estimator, level = 0.95, ...) {
 #' @param save Logical; Whether or not to save the plot to disk; FALSE by default.
 #' @param path String; File directory to save file.
 #' @param ... arguments to pass to `ggplot2:geom_*` function (e.g. `color`, `linewidth`, `alpha`, `bins` etc.)
-#' @param standard_error String; type of standard error to use, the "resampled" distribution or the "normal" approximation. Only
-#' used when type = "estimate".
+#' @param cdf String; specifies the type of CDF to use when analyzing the estimates.
+#' valid cdfs are the `empirical` cdf, the `normal` cdf. Used when type = `estimate`.
 #' @inheritParams summary.multiple.mab
 #' @export
 #' @returns Minimal ggplot object, that can be customized and added to with `+` (To change, scales, labels, legend, theme, etc.)
@@ -145,10 +145,10 @@ plot_estimates <- function(x, estimator, level = 0.95, ...) {
 plot.multiple.mab <- function(x, type, estimator = NULL, standard_error = NULL, level = 0.95, save = FALSE, path = NULL, ...) {
   plot <- switch(type,
     "summary" = plot_summary(x = x, ...),
-    "hist" = plot_hist(x = x, estimator = estimator),
+    "hist" = plot_hist(x = x, estimator = estimator, ...),
     "estimate" = plot_mult_estimates(
       x = x, estimator = estimator, standard_error = standard_error,
-      level = level
+      level = level, ...
     ),
     rlang::abort("Invalid Type: Valid types are `hist`, `summary`, estimate`.")
   )
@@ -169,6 +169,7 @@ plot.multiple.mab <- function(x, type, estimator = NULL, standard_error = NULL, 
 
 plot_summary <- function(x, ...) {
   summary(x) |>
+    dplyr::filter(estimator == "AIPW") |>
     ggplot2::ggplot(ggplot2::aes(x = mab_condition, y = times_best)) +
     ggplot2::geom_bar(stat = "identity", ...) +
     ggplot2::labs(
@@ -199,6 +200,7 @@ plot_hist <- function(x, estimator, ...) {
   )
 
   x$estimates |>
+    dplyr::filter(estimator %in% estimator_arg) |>
     ggplot2::ggplot(ggplot2::aes(x = mean, y = ggplot2::after_stat(density))) +
     ggplot2::geom_histogram(...) +
     ggplot2::facet_grid(~ mab_condition + estimator) +
@@ -218,8 +220,10 @@ plot_hist <- function(x, estimator, ...) {
 #' @returns Minimal ggplot object, that can be customized and added to with `+` (To change, scales, labels, legend, theme, etc.)
 
 
-plot_estimates <- function(x, estimator, standard_error, level, ...) {
-  if (base::is.null(estimator)) rlang::abort("Invalid Estimator: Valid Estimators are `both`, `AIPW`, and `Sample`")
+plot_mult_estimates <- function(x, estimator, cdf, level, ...) {
+  if (base::is.null(estimator)) {
+    rlang::abort("Invalid Estimator: Valid Estimators are `both`, `AIPW`, and `Sample`")
+  }
 
   estimator_arg <- switch(estimator,
     "Both" = c("Sample", "AIPW"),
@@ -227,5 +231,26 @@ plot_estimates <- function(x, estimator, standard_error, level, ...) {
     "Sample" = c("Sample"),
     rlang::abort("Invalid Estimator: Valid Estimators are `both`, `AIPW`, and `Sample`")
   )
-  return(0)
+
+  if (base::is.null(cdf)) {
+    rlang::abort("Invalid Estimator: Valid Estimators are, empirical`, and `normal`")
+  }
+  cols <- switch(cdf,
+    ,
+    "empirical" = c("upper_empirical", "lower_empirical"),
+    "normal" = c("upper_normal", "lower_normal")
+  )
+
+  summary(x, level = level) |>
+    dplyr::filter(estimator %in% estimator_arg) |>
+    dplyr::select(tidyselect::all_of(cols), estimator, mab_condition, estimate_avg) |>
+    ggplot2::ggplot(ggplot2::aes(x = estimate_avg, y = mab_condition)) +
+    ggplot2::geom_errorbarh(ggplot2::aes_string(xmin = cols[[1]], xmax = cols[[2]])) +
+    ggplot2::facet_grid(~estimator) +
+    ggplot2::theme_minimal() +
+    ggplot2::labs(
+      x = "Estimate", y = "Treatment Arm",
+      title = "Uncertainy Around Treatment Arm Estimates"
+    ) +
+    ggplot2::theme(panel.spacing = ggplot2::unit(2, "lines"))
 }
