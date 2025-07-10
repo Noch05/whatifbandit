4b4d33d8e5ea111d39bc09d7b147da2e2fa0bc1e
Noah Ochital <noahochital@icloud.com>
2025-07-10 14:40:44 -0400

    Fixing Test to check proper error is called

---
+++ tests/testthat/_snaps/single_mab_simulation.md
@@ -0,0 +1,53 @@
+# Throws Proper Error when ID's are not unique
+
+    id is not a unique identifier, a unique id for each observation is required
+
+# Columns that do not exist in data are found
+
+    i In index: 3.
+    Caused by error in `.f()`:
+    ! Required column `condition_col` is not found in provided `data`.
+    x reason: it is always required
+
+---
+
+    i In index: 1.
+    Caused by error in `.f()`:
+    ! Required column `id_col` is not found in provided `data`.
+    x reason: it is always required
+
+---
+
+    i In index: 1.
+    Caused by error in `.f()`:
+    ! Required column `id_col` is not found in provided `data`.
+    x reason: it is always required
+
+---
+
+    i In index: 2.
+    Caused by error in `.f()`:
+    ! Required column `success_col` is not found in provided `data`.
+    x reason: it is always required
+
+---
+
+    i In index: 2.
+    Caused by error in `.f()`:
+    ! Required column `success_col` is not found in provided `data`.
+    x reason: it is always required
+
+---
+
+    i In index: 1.
+    Caused by error in `.f()`:
+    ! Required column `id_col` is not found in provided `data`.
+    x reason: it is always required
+
+---
+
+    i In index: 1.
+    Caused by error in `.f()`:
+    ! Required column `id_col` is not found in provided `data`.
+    x reason: it is always required
+
--- tests/testthat/test-single_mab_simulation.R
+++ tests/testthat/test-single_mab_simulation.R
@@ -1,10 +1,11 @@
 test_that("Throws Proper Error when ID's are not unique", {
   data(tanf)
+  tanf <- tanf[sample.int(15, 100, replace = TRUE), ]
   conditions <- setNames(
     c("no_letter", "open_appt", "specific_appt"),
     c("Control", "T1", "T2")
   )
-  expect_error(single_mab_simulation(
+  expect_snapshot_error(single_mab_simulation(
     data = tanf,
     assignment_method = "Individual",
     whole_experiment = TRUE,
@@ -16,23 +17,32 @@ test_that("Throws Proper Error when ID's are not unique", {
     data_cols = c(
       condition_col = "condition",
       id_col = "id",
-      success_col = "success",
+      success_col = "success"
     ), verbose = TRUE
   ))
 })
-# Making Id's Unique
-test_that("Column Checking Works Properly", {
+
+
+test_that("Columns that do not exist in data are found", {
   col_args <- expand.grid(
     condition_col = c(condition_col = "condition", condition_col = "fake_colname"),
     id_col = c(id_col = "id", id_col = "fake_colname"),
-    success_col = c(success_col = "success", success_col = "fake_colname")
+    success_col = c(success_col = "success", success_col = "fake_colname"),
+    stringsAsFactors = FALSE
   ) |>
-    slice(-1)
+    dplyr::rowwise() |>
+    dplyr::mutate(fake_count = sum(dplyr::c_across(dplyr::everything()) == "fake_colname")) |>
+    dplyr::filter(fake_count == 1) |>
+    dplyr::ungroup() |>
+    select(-fake_count)
 
-  data(tanf)
+  purrr::walk(seq_len(nrow(col_args)), ~ {
+    data_cols <- setNames(
+      as.character(col_args[.x, ]),
+      names(col_args)
+    )
 
-  purrr::walk(col_args, ~ {
-    expect_error(single_mab_simulation(
+    expect_snapshot_error(single_mab_simulation(
       data = tanf,
       assignment_method = "Individual",
       whole_experiment = TRUE,
@@ -41,7 +51,7 @@ test_that("Column Checking Works Properly", {
       algorithm = "Thompson",
       prior_periods = "All",
       conditions = conditions,
-      data_cols = c(.x)
+      data_cols = data_cols
     ))
   })
 })
