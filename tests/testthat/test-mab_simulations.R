2de9557c4410eac204ad6e6e9e8e9296aed32cbb
Noah Ochital <noahochital@icloud.com>
2025-07-11 00:01:16 -0400

    Removing aes_string' fixing a doc typo; removing file that broke tests

--- tests/testthat/test-mab_simulations.R
+++ tests/testthat/test-mab_simulations.R
@@ -1,11 +1,9 @@
 test_that("BaseR/Dplyr Single Simulation Test", {
-  ## Using strange Names to Replicate potential names
-  ## in a real data set
   set.seed(5832601)
   required_inputs <- generate_data(100)
   full_args <- required_inputs$full_args
   static_args <- required_inputs$static_args
-  run_test(full_args, static_args)
+  run_test(full_args, static_args, trial = "single")
 })
 
 test_that("data.table Single Simulation Test", {
@@ -14,24 +12,31 @@ test_that("data.table Single Simulation Test", {
   full_args <- required_inputs$full_args
   static_args <- required_inputs$static_args
   static_args$data <- data.table::setDT(static_args$data)
-  run_test(full_args, static_args)
+
+  run_test(full_args, static_args, trial = "single")
 })
 
-test_that("BaseR/Dplyr Single Simulation Test", {
-  ## Using strange Names to Replicate potential names
-  ## in a real data set
-  set.seed(5832601)
+test_that("BaseR/Dplyr Multiple Simulation Test", {
+  set.seed(043932)
   required_inputs <- generate_data(100)
   full_args <- required_inputs$full_args
   static_args <- required_inputs$static_args
-  run_test(full_args, static_args)
+  static_args$times <- 2
+  static_args$seeds <- sample.int(100, 2)
+  static_args$keep_data <- FALSE
+
+  run_test(full_args, static_args, trial = "multiple")
 })
 
-test_that("data.table Single Simulation Test", {
-  set.seed(1235493)
+test_that("data.table Multiple Simulation Test", {
+  set.seed(494873)
   required_inputs <- generate_data(100)
   full_args <- required_inputs$full_args
   static_args <- required_inputs$static_args
+  static_args$times <- 2
+  static_args$seeds <- sample.int(100, 2)
+  static_args$keep_data <- FALSE
   static_args$data <- data.table::setDT(static_args$data)
-  run_test(full_args, static_args)
+
+  run_test(full_args, static_args, trial = "multiple")
 })
