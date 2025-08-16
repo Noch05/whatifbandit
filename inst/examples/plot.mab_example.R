2bbf3b2a13c4c67639a7da7ee31dd0f0d8e092fc
Noah Ochital <noahochital@icloud.com>
2025-08-16 16:49:28 -0400

    Importing ggplot2

--- inst/examples/plot.mab_example.R
+++ inst/examples/plot.mab_example.R
@@ -1,44 +1,41 @@
 # Objects returned by `single_mab_simulation()` have a `mab` class.
 # This class has a plot generic that has several minimal plots to examine
 # the trial quickly
-#
-# These functions require ggplot2
-if (requireNamespace("ggplot2", quietly = TRUE)) {
-  # Loading Data and running a quick simulation
-  data(tanf)
-  x <- single_mab_simulation(
-    data = tanf,
-    algorithm = "Thompson",
-    assignment_method = "Batch",
-    period_length = 600,
-    whole_experiment = TRUE,
-    perfect_assignment = TRUE,
-    blocking = FALSE,
-    prior_periods = "All",
-    conditions = c(
-      "no_letter",
-      "open_appt",
-      "specific_appt"
-    ),
-    data_cols = c(
-      condition_col = "condition",
-      id_col = "ic_case_id",
-      success_col = "success"
-    )
+
+# Loading Data and running a quick simulation
+data(tanf)
+x <- single_mab_simulation(
+  data = tanf,
+  algorithm = "Thompson",
+  assignment_method = "Batch",
+  period_length = 600,
+  whole_experiment = TRUE,
+  perfect_assignment = TRUE,
+  blocking = FALSE,
+  prior_periods = "All",
+  conditions = c(
+    "no_letter",
+    "open_appt",
+    "specific_appt"
+  ),
+  data_cols = c(
+    condition_col = "condition",
+    id_col = "ic_case_id",
+    success_col = "success"
   )
+)
 
-  # View best treatment arms over the simulation
-  y <- plot(x, type = "arm")
-  y
-  # Adding a new title
-  y + ggplot2::labs(title = "Your New Title")
-  # type = assign creates a similar plot, but shows probability of assignment instead
+# View best treatment arms over the simulation
+y <- plot(x, type = "arm")
+y
+# Adding a new title
+y + ggplot2::labs(title = "Your New Title")
+# type = assign creates a similar plot, but shows probability of assignment instead
 
 
-  # Plotting Augmented Inverse Probability Estimates with confidence interval
-  # By default it provides 95% Normal Confidence Intervals but this can be adjusted
-  plot(x, type = "estimate")
+# Plotting Augmented Inverse Probability Estimates with confidence interval
+# By default it provides 95% Normal Confidence Intervals but this can be adjusted
+plot(x, type = "estimate")
 
-  # Adjusting height of internal geom* argument. (`geom_errorbarh()`)
-  plot(x, type = "estimate", height = 0.4)
-}
+# Adjusting height of internal geom* argument. (`geom_errorbarh()`)
+plot(x, type = "estimate", height = 0.4)
--- inst/examples/plot.multiple.mab_example.R
+++ inst/examples/plot.multiple.mab_example.R
@@ -2,46 +2,44 @@
 # This class has a plot generic has several minimal plots to examine the trials
 # quickly
 #
-# # These functions require ggplot2
-if (requireNamespace("ggplot2", quietly = TRUE)) {
-  data(tanf)
-  tanf <- tanf[1:20, ]
-  # Simulating a few trials
+#
+data(tanf)
+tanf <- tanf[1:20, ]
+# Simulating a few trials
 
-  seeds <- sample.int(100, 5)
-  conditions <- as.character(unique(tanf$condition))
-  x <- multiple_mab_simulation(
-    data = tanf,
-    assignment_method = "Batch",
-    period_length = 10,
-    whole_experiment = TRUE,
-    blocking = FALSE,
-    perfect_assignment = TRUE,
-    algorithm = "Thompson",
-    prior_periods = "All",
-    control_augment = 0,
-    conditions = conditions,
-    data_cols = c(
-      condition_col = "condition",
-      id_col = "ic_case_id",
-      success_col = "success"
-    ),
-    verbose = FALSE, times = 5, seeds = seeds, keep_data = FALSE
-  )
+seeds <- sample.int(100, 5)
+conditions <- as.character(unique(tanf$condition))
+x <- multiple_mab_simulation(
+  data = tanf,
+  assignment_method = "Batch",
+  period_length = 10,
+  whole_experiment = TRUE,
+  blocking = FALSE,
+  perfect_assignment = TRUE,
+  algorithm = "Thompson",
+  prior_periods = "All",
+  control_augment = 0,
+  conditions = conditions,
+  data_cols = c(
+    condition_col = "condition",
+    id_col = "ic_case_id",
+    success_col = "success"
+  ),
+  verbose = FALSE, times = 5, seeds = seeds, keep_data = FALSE
+)
 
-  # View number of times each treatment was the best.
-  plot(x, type = "summary")
+# View number of times each treatment was the best.
+plot(x, type = "summary")
 
-  # View a histogram of the AIPW estimates for each treatment.
-  plot(x, type = "hist")
+# View a histogram of the AIPW estimates for each treatment.
+plot(x, type = "hist")
 
-  # Plotting AIPW confidence intervals using the empirical cdf, from the simulated
-  # trials.
-  plot(x, type = "estimate", cdf = "empirical")
+# Plotting AIPW confidence intervals using the empirical cdf, from the simulated
+# trials.
+plot(x, type = "estimate", cdf = "empirical")
 
-  # Changing the title, like any ggplot2 object.
-  plot(x, type = "summary") + ggplot2::labs(title = "Your New Title")
+# Changing the title, like any ggplot2 object.
+plot(x, type = "summary") + ggplot2::labs(title = "Your New Title")
 
-  # Changing the bin width of the histogram.
-  plot(x, type = "hist", binwidth = 0.05)
-}
+# Changing the bin width of the histogram.
+plot(x, type = "hist", binwidth = 0.05)
