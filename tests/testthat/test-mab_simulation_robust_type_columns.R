a8d8908bf57fc225a5a36ff9ff5d091482e13658
Noah Ochital <noahochital@icloud.com>
2025-08-18 18:51:08 -0400

    using data.table namespace to reference functions, fixing errors when package is not attached

--- tests/testthat/test-mab_simulation_robust_type_columns.R
+++ tests/testthat/test-mab_simulation_robust_type_columns.R
@@ -15,7 +15,7 @@ test_that("Data.table datatype test", {
   required_inputs <- generate_data(100, 3)
   full_args <- required_inputs$full_args
   static_args <- required_inputs$static_args
-  static_args$data <- data.table(static_args$data)
+  static_args$data <- data.table::data.table(static_args$data)
   static_args$data$type_of_policy <- as.factor(static_args$data$type_of_policy)
   run_test(full_args, static_args, trial = "single")
   static_args$data$type_of_policy <- as.numeric(static_args$data$type_of_policy)
